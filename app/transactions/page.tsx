0b053a02ced3acd83d7ff46428b2143c056566e6
ezequiel <falecomezequielcardoso@gmail.com>
2025-01-02 18:14:23 -0300

    feat: add authentication check and redirect for Subscription and Transactions pages

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -3,9 +3,22 @@ import { transactionsColumns } from "./_columns";
 import { db } from "../_lib/prisma";
 import AddTransactionButton from "../_components/add-transaction-button";
 import Navbar from "../_components/navbar";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
 const TransactionsPage = async () => {
-  const transactions = await db.transaction.findMany();
+  const { userId } = await auth();
+  if (!userId) {
+    redirect("/login");
+  }
+  const transactions = await db.transaction.findMany({
+    where: {
+      userId,
+    },
+    orderBy: {
+      date: "desc",
+    },
+  });
   return (
     <>
       <Navbar />
