027063f35ff3a12620aa83b1119d635d58a391eb
ezequiel <falecomezequielcardoso@gmail.com>
2025-01-03 16:31:13 -0300

    feat: implement subscription management with Stripe integration and transaction limits

--- app/subscription/_components/acquire-plan-button.tsx
+++ app/subscription/_components/acquire-plan-button.tsx
@@ -1,25 +1,25 @@
 "use client";
 
 import { Button } from "@/app/_components/ui/button";
-//import { createStripeCheckout } from "../_actions/create-stripe-checkout";
-//import { loadStripe } from "@stripe/stripe-js";
+import { createStripeCheckout } from "../_actions/create-stripe-checkout";
+import { loadStripe } from "@stripe/stripe-js";
 import { useUser } from "@clerk/nextjs";
 import Link from "next/link";
 
 const AcquirePlanButton = () => {
   const { user } = useUser();
   const handleAcquirePlanClick = async () => {
-    //const { sessionId } = await createStripeCheckout();
-    //if (!process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY) {
-    //   throw new Error("Stripe publishable key not found");
-    // }
-    // const stripe = await loadStripe(
-    //   process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
-    // );
-    // if (!stripe) {
-    //   throw new Error("Stripe not found");
-    // }
-    // await stripe.redirectToCheckout({ sessionId });
+    const { sessionId } = await createStripeCheckout();
+    if (!process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY) {
+      throw new Error("Stripe publishable key not found");
+    }
+    const stripe = await loadStripe(
+      process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
+    );
+    if (!stripe) {
+      throw new Error("Stripe not found");
+    }
+    await stripe.redirectToCheckout({ sessionId });
   };
   const hasPremiumPlan = user?.publicMetadata.subscriptionPlan == "premium";
   if (hasPremiumPlan) {
